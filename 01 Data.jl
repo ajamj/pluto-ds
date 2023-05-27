cc026f0569e02c517e27661902272af978b9eb09
Ajam Jamaludin <jamaludinajam@gmail.com>
2023-05-27 17:06:05 +0700

    Initial commit

---
+++ 01 Data.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c36ab6b0-fc74-11ed-0562-b7c658062add
+
+
+# ╔═╡ Cell order:
+# ╠═c36ab6b0-fc74-11ed-0562-b7c658062add
---
+++ README.md
@@ -0,0 +1,2 @@
+# pluto-data-science
+ 
